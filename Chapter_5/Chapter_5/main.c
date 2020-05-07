805727a5f9d755a4f0d2e8c321d2fafe8ccdfd1f
DuLiCS <ieee811@gmail.com>
2020-05-07 12:56:43 +0800

    1

---
+++ Chapter_5/Chapter_5/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Chapter_5
+//
+//  Created by 杜立 on 2020/5/7.
+//  Copyright © 2020 杜立. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+void pro5_1(){
+    char *s = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+}
+
